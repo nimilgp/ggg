374ad8f0f7931d54d6f5cb72c4d1e87ae6825235
nimilgp <nimilgeorgepradeepngp@gmail.com>
2025-10-16 21:33:19 +0530

    basic structure

---
+++ app/cmd/api/api.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthcheck", app.healthcheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Print("Starting server on ", app.config.addr)
+
+	return srv.ListenAndServe()
+}
